697cf6554f42f4e476b6101769b2537cf4b8304d
duongnh <hongyanggreat@gmail.com>
2018-05-20 17:02:04 +0700

    stream thanh cong

--- src/openStream.js
+++ src/openStream.js
@@ -1,9 +1,30 @@
 const playVideo = require('./playVideo');
-
-function openStream(){
+const Peer = require('simple-peer');
+const $ = require('jquery');
+function openStream() {
 	navigator.mediaDevices.getUserMedia({ audio: false, video: true })
-	.then(stream => playVideo(stream,"localStream"))
-	.catch(err => console.log(err));
+		.then(stream => {
+			playVideo(stream, "localStream");
+
+			const p = new Peer({
+				initiator: location.hash === "#1",
+				trickle: false,
+				stream,
+
+			});
+			// console.log(222);
+			p.on('signal', token => {
+				$('#txtMySignal').val(JSON.stringify(token))
+			});
+			p.on('stream', friendStream => playVideo(friendStream, "friendStream"));
+
+			$('#btnConnect').click(() => {
+				;
+				const friendSignal = JSON.parse($('#txtFriendSignal').val())
+				p.signal(friendSignal);
+			});
+		})
+		.catch(err => console.log(err));
 }
 
 module.exports = openStream;
