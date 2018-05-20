66466c05d1e4aca322e23587c3067fb0a165b669
duongnh <hongyanggreat@gmail.com>
2018-05-20 17:13:39 +0700

    tach file js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,28 @@
 const openStream = require('./openStream');
+const playVideo = require('./playVideo');
+const Peer = require('simple-peer');
 const $ = require('jquery');
-openStream();
+openStream(function (stream) {
+    playVideo(stream, "localStream");
+
+    const p = new Peer({
+        initiator: location.hash === "#1",
+        trickle: false,
+        stream,
+
+    });
+    p.on('signal', token => {
+        $('#txtMySignal').val(JSON.stringify(token))
+    });
+
+    $('#btnConnect').click(() => {
+        ;
+        const friendSignal = JSON.parse($('#txtFriendSignal').val())
+        p.signal(friendSignal);
+    });
+
+    p.on('stream', friendStream => playVideo(friendStream, "friendStream"));
+});
 
 
 // console.log("resolve : How to move file main.js from dist to public");
--- src/openStream.js
+++ src/openStream.js
@@ -1,28 +1,11 @@
 const playVideo = require('./playVideo');
 const Peer = require('simple-peer');
 const $ = require('jquery');
-function openStream() {
+function openStream(cb) {
+
 	navigator.mediaDevices.getUserMedia({ audio: false, video: true })
 		.then(stream => {
-			playVideo(stream, "localStream");
-
-			const p = new Peer({
-				initiator: location.hash === "#1",
-				trickle: false,
-				stream,
-
-			});
-			// console.log(222);
-			p.on('signal', token => {
-				$('#txtMySignal').val(JSON.stringify(token))
-			});
-			p.on('stream', friendStream => playVideo(friendStream, "friendStream"));
-
-			$('#btnConnect').click(() => {
-				;
-				const friendSignal = JSON.parse($('#txtFriendSignal').val())
-				p.signal(friendSignal);
-			});
+			cb(stream);
 		})
 		.catch(err => console.log(err));
 }
